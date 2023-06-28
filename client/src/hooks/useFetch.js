0c9f7d159131afc847024d7513fa41d0e8ba8611
M V Midhun <midhunmareedu@gmail.com>
2023-06-28 18:13:33 +0530

    Modified  the client for containing react app

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -4,22 +4,18 @@ import api from "../api";
 
 const useFetch = () => {
 
-  const fetchData = useCallback(async (config, otherOptions) => {
+  const fetchData = useCallback(async (config) => {
 
-    const { showSuccessToast = true, showErrorToast = true } = otherOptions || {};
 
     try {
       const { data } = await api.request(config);
-      // console.log(data)
-      if (showSuccessToast) toast.success(data.msg);
-      return Promise.resolve(data);   
+      return data;   
     }
     catch (error) {
       const msg = error.response?.data?.msg || error.message || "error";
-      if (showErrorToast) toast.error(msg);
-      return Promise.reject();
+      return msg;
     }
-  }, []);
+  },[]);
 
   return fetchData;
 }
